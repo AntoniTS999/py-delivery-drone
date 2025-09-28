c15b39e7653af3b997128dd74630dcafd079c741
Anton Tsikhanovich <alluspeh@gmail.com>
2025-09-28 20:59:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(self,
+                 name: str,
+                 weight: int,
+                 coords: list | None = None) -> None:
         self.name = name
         self.weight = weight
+        coords = coords or [0, 0]
         self.coords = coords
-        if self.coords is None:
-            self.coords = [0, 0]
 
     def go_forward(self, steps: int = 1) -> None:
         self.coords[1] += steps
@@ -23,7 +25,7 @@ class BaseRobot:
     def go_left(self, steps: int = 1) -> None:
         self.coords[0] -= steps
 
-    def get_info(self) -> None:
+    def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
 
 
@@ -32,9 +34,8 @@ class FlyingRobot(BaseRobot):
                  name: str,
                  weight: int,
                  coords: list = None) -> None:
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
-        if len(self.coords) <= 2:
-            self.coords.append(0)
 
     def go_up(self, steps: int = 1) -> None:
         self.coords[2] += steps
@@ -47,12 +48,15 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self,
                  name: str,
                  weight: int,
-                 max_load_weight: int,
-                 coords: list = None,
-                 current_load: Cargo = None) -> None:
+                 coords: list | None = None,
+                 max_load_weight: int = None,
+                 current_load: Cargo | None = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        self.current_load = None
+
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
